5d44a81c30940162fd028298d1b70d71b21dca65
Kirill Martynov <matifax1990@yandex.ru>
2025-12-28 04:01:11 +0500

    lastday?

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,25 @@
 package actioninfo
 
+import "fmt"
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, data := range dataset {
+		if err := dp.Parse(data); err != nil {
+			fmt.Println("Ошибка:", err)
+			continue
+		}
+
+		info, err := dp.ActionInfo()
+		if err != nil {
+			fmt.Println("Ошибка:", err)
+			continue
+		}
+
+		fmt.Println(info)
+	}
 }
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -1,29 +1,49 @@
 package spentenergy
 
 import (
+	"errors"
 	"time"
 )
 
-// Основные константы, необходимые для расчетов.
 const (
-	mInKm                      = 1000 // количество метров в километре.
-	minInH                     = 60   // количество минут в часе.
-	stepLengthCoefficient      = 0.45 // коэффициент для расчета длины шага на основе роста.
-	walkingCaloriesCoefficient = 0.5  // коэффициент для расчета калорий при ходьбе.
+	mInKm                      = 1000
+	minInH                     = 60
+	stepLengthCoefficient      = 0.45
+	walkingCaloriesCoefficient = 0.5
 )
 
-func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
-}
-
-func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
+func Distance(steps int, height float64) float64 {
+	if steps <= 0 || height <= 0 {
+		return 0
+	}
+	stepLength := height * stepLengthCoefficient
+	return float64(steps) * stepLength / mInKm
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
-	// TODO: реализовать функцию
+	if steps <= 0 || height <= 0 || duration <= 0 {
+		return 0
+	}
+	dist := Distance(steps, height)
+	hours := duration.Hours()
+	return dist / hours
 }
 
-func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
+func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, errors.New("invalid input data")
+	}
+	speed := MeanSpeed(steps, height, duration)
+	minutes := duration.Minutes()
+	return (weight * speed * minutes) / minInH, nil
+}
+
+func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, errors.New("invalid input data")
+	}
+	speed := MeanSpeed(steps, height, duration)
+	minutes := duration.Minutes()
+	cal := (weight * speed * minutes) / minInH
+	return cal * walkingCaloriesCoefficient, nil
 }
